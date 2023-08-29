29812c04a4757a8ab896eca682f01c3270c5cb49
MrTESSARD <dimitri@tessard.pro>
2023-08-29 13:33:10 +0200

    Readmy

--- src/app/api/session/route.ts
+++ src/app/api/session/route.ts
@@ -5,14 +5,13 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
   const session = await getServerSession(authOptions);
 
-//Protéger une route API
+  //Protéger une route API
   if (!session) {
     return new NextResponse(
       JSON.stringify({ status: "fail", message: "You are not logged in" }),
       { status: 401 }
     );
   }
-
 
   return NextResponse.json({
     authenticated: !!session,
